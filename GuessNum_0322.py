427801b98c6350796c9709e5aa5e45aedd67e370
Joker2U <yngwieli10@gmail.com>
2026-03-22 10:44:26 +0800

    版本v2

--- GuessNum_0322.py
+++ GuessNum_0322.py
@@ -1,13 +1,17 @@
 import random
 i = random.randint(1,10)
+count = 0
 while True:
+    count += 1
     num = int(input("請輸入數字:"))
     if num == i:
         print("猜對了")
+        print(f"這是你猜的第{count}次")
         break
     elif num < i:
         print("你猜得比答案小")
     else :
         print("你猜得比答案大")
+    print(f"這是你猜的第{count}次")
 
 
